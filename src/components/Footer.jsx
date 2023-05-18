771e7dd86f8f8de7c9d0691f3a013581821af454
Alexandro Diaz <diazalex4589@gmail.com>
2023-05-18 17:57:00 -0400

    footer has h3 text in it now

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,13 @@
-const Footer = () => {}
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h3>
+        {' '}
+        All information was pulled from wikipedia and was fact checked by fellow
+        car enthusiast in the area that work with said motors. All photos are
+        pulled from website and store fronts that sell these engines already
+        built, cosmetics, or kits to make more horsepower.
+      </h3>
+    </div>
+  )
+}
